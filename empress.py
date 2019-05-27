6c53418a8ef42041bfe237378c216c318fb2fe50
Barry Van Deerlin <bvandeerlin@gmail.com>
2019-05-26 21:34:20 -0700

    properly indented a code block that was misindented

--- empress.py
+++ empress.py
@@ -18,6 +18,29 @@ import argparse
 import RPi.GPIO as GPIO
 from time import sleep
 
+#     Raspberry Pi Pinout
+#      3V3  (1)  (2) 5V
+#    GPIO2  (3)  (4) 5V
+#    GPIO3  (5)  (6) GND
+#    GPIO4  (7)  (8) GPIO14
+#      GND  (9) (10) GPIO15
+#   GPIO17 (11) (12) GPIO18
+#   GPIO27 (13) (14) GND
+#   GPIO22 (15) (16) GPIO23
+#      3V3 (17) (18) GPIO24
+#   GPIO10 (19) (20) GND
+#    GPIO9 (21) (22) GPIO25
+#   GPIO11 (23) (24) GPIO8
+#      GND (25) (26) GPIO7
+#    GPIO0 (27) (28) GPIO1
+#    GPIO5 (29) (30) GND
+#    GPIO6 (31) (32) GPIO12
+#   GPIO13 (33) (34) GND
+#   GPIO19 (35) (36) GPIO16
+#   GPIO26 (37) (38) GPIO20
+#      GND (39) (40) GPIO21
+
+
 # System Targets List
 systems = ["system1", "system2"]
 # System Power Pin List
@@ -60,11 +83,11 @@ group.add_argument(
     help='Flag indicates Power Off  Target System')
 
 group.add_argument(
-     '--reset', '-r',
-     action='store_true',
-     dest="reset",
-     default=False,
-     help='Flag indicates Reset Target System')
+    '--reset', '-r',
+    action='store_true',
+    dest="reset",
+    default=False,
+    help='Flag indicates Reset Target System')
 
 parser.add_argument(
     '--version', '-v',
